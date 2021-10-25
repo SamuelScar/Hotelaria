69cda0c4d5dfb613c115360347643b4df80222f6
Samuel Souza <souzacarvalhosamuel@gmail.com>
2021-10-25 12:29:57 -0300

    Realizado alterações no parse para retirar as exceções, começado a criar validação e moteodo que faz check-in no periodo determinado pelo usuario.

--- src/Apartment.java
+++ src/Apartment.java
@@ -1,3 +1,6 @@
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Apartment {
@@ -9,8 +12,30 @@ public class Apartment {
     private boolean occupation;
     Date [] entry_dates =  new Date[TAM_DATAS];
     Date [] departure_dates =  new Date[TAM_DATAS];
+    ParsePosition position = new ParsePosition(0);
 
     public Apartment(){
 
+    }
+
+    public boolean validaData(Date dateEntry,Date dateOut) {
+
+        boolean validade = false;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        // Formata data de entrada
+        String auxDate = dateFormat.format(dateEntry);
+        dateEntry = dateFormat.parse(auxDate,position);
+
+        // Formata data de saida
+        auxDate = dateFormat.format(dateOut);
+        dateOut = dateFormat.parse(auxDate,position);
+
+        //Criar metodos para validar a data prevista usando o array de datas
+        if(dateEntry.before(dateOut)){
+
+        }
+
+        return validade;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,13 @@ import java.util.Date;
 public class Main {
 
     final static int NUM_HOSP = 100;
+    final static int NUM_APART = 10;
     private static Hospede[] hospedes = new Hospede[NUM_HOSP];
     private static int numHospedes = 0;
     private static SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy");
+    private static Apartment[] apartments = new Apartment[NUM_APART];
+    private static int numApartments = 0;
+    private static ParsePosition position = new ParsePosition(0);
 
     public static void main(String[] args) throws ParseException {
 
@@ -49,9 +53,10 @@ public class Main {
         numHospedes++;
     }
 
-    public static void checkIn() throws ParseException {
+    public static void checkIn(){
 
         int codClient, codApartament, numberHospedes, previsaoSaida, i=0;
+        boolean validade = false;
         String auxDate;
         Date dateEntry, dateOut = new Date();
 
@@ -63,18 +68,26 @@ public class Main {
 
         System.out.println("Data de entrada (format _/_/_): ");
         auxDate = Console.readLine();
-        dateEntry = simpleDate.parse(auxDate);
-
+        dateEntry = simpleDate.parse(auxDate,position);
+        System.out.println(dateEntry);
         System.out.println("Data de saida (format _/_/_): ");
         auxDate = Console.readLine();
-        dateOut = simpleDate.parse(auxDate);
+        dateOut = simpleDate.parse(auxDate,position);
 
-        validarCheckIn(codClient,codApartament,dateEntry,dateOut);
+        validade = validarCheckIn(codClient,codApartament,dateEntry,dateOut);
     }
 
-    public static void validarCheckIn(int codClient, int codApartament, Date dateEntry, Date dateOut){
+    public static boolean validarCheckIn(int codClient, int codApartament, Date dateEntry, Date dateOut){
 
         //fazer validação dos dados para executar o checkIn
 
+        boolean validade = false;
+
+        if(!apartments[codApartament].validaData(dateEntry,dateOut)){
+            return validade;
+        }
+        //criar os outros metodos de validação
+
+        return validade;
     }
 }
